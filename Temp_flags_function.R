64c73c133417949ad66d480d8819a0f2d7a4ef6f
Rebecca Shaftel <rsshaftel@alaska.edu>
2019-09-05 12:33:49 -0800

    Finished function that add flags for questionable temperatures to datasets. Function will work on continuous data for one site. It attributes dates with a flag for four types of readings: - high temps, default set to 25 - low temps, default set to -1 - fast change in hourly temps, default set to 3 - fast change in daily temps, default set to 3 Also wrote a short function for plotting the flags in four separate plots on one page.

--- Temp_flags_function.R
+++ Temp_flags_function.R
@@ -1,64 +1,117 @@
 
+library(gridExtra)
 
-add_temp_flags <- function(filename, site_column_name) {
-  raw <- read_csv(filename)
-  
-  rawMets <- raw %>% 
-    group_by(hour, site_column_name) %>% 
-    summarize(hourlyMean = mean(Temperature, na.rm = TRUE),
-              hourlyMax = max(Temperature, na.rm = TRUE)) %>% 
-    mutate(hourlyMeanDiff = c(0, abs(diff(dailyMean))),
-           hourlyMaxDiff = c(0, abs(diff(dailyMax))))
-  
-  sitehourFlags <- rawMets %>% 
-    filter(hourlyMeanDiff > 3 | hourlyMaxDiff > 3) %>% 
-    pull(hour, sampleDate)
-  
-  dailyMets <- raw %>% 
-    group_by(sampleDate, site_column_name) %>% 
-    summarize(dailyMean = mean(Temperature, na.rm = TRUE),
-              dailyMax = max(Temperature, na.rm = TRUE)) %>% 
-    mutate(dailyMeanDiff = c(0, abs(diff(dailyMean))),
-           dailyMaxDiff = c(0, abs(diff(dailyMax))))
-  
-  sitedateFlags <- dailyMets %>% 
-    filter(dailyMeanDiff > 3 | dailyMaxDiff > 3) %>% 
+#Start by writing functions for each type of flag.
+#Each function should create a new column (1/0) for the flag.
+
+#Each function will need a data input and fields named 
+# 1) Temperature - numeric
+# 2) sampleDate - date
+# 3) sampleTime - hms
+
+#All flags find errors in the time series by date and apply
+# a flag (1) for all measurements on that date.
+
+
+flag_high_temps <- function(data, high_temp_value = 25) {
+  dates_to_flag <- data %>% 
+    filter(Temperature > high_temp_value) %>% 
+    distinct(sampleDate) %>% 
     pull(sampleDate)
-
-  diff(dailyMets$dailyMean, lag = 1)
-  
-  raw %>% 
-    mutate(flag = case_when(Temperature > 30 ~ 1,
-                            Temperature < -1 ~ 2,
-                            
-           )
-  
-  
-  #final value returned as result, don't need to assign.
+  data %>% 
+    mutate(flag_highTemps = ifelse(sampleDate %in% dates_to_flag, 1, 0))
 }
 
-# Update the function so heads have probability p_head
-toss_coin <- function(n_flips, p_head = 0.5) {
-  coin_sides <- c("head", "tail")
-  # Define a vector of weights
-  weights <- c(p_head, 1-p_head)
-  # Modify the sampling to be weighted
-  sample(coin_sides, n_flips, replace = TRUE, prob = weights)
+flag_low_temps <- function(data, low_temp_value = -1) {
+  dates_to_flag <- data %>% 
+    filter(Temperature < low_temp_value) %>% 
+    distinct(sampleDate) %>% 
+    pull(sampleDate)
+  data %>% 
+    mutate(flag_lowTemps = ifelse(sampleDate %in% dates_to_flag, 1, 0))
 }
 
-# Generate 10 coin tosses
-toss_coin(10, 0.8)
+flag_hourly_change <- function(data, hourly_change_value = 3) {
+  dates_to_flag <- data %>% 
+    group_by(sampleDate, as.POSIXct(round(dateTime, units = "hours"))) %>% 
+    summarize(hourlyMeanTemp = mean(Temperature, na.rm = TRUE)) %>% 
+    mutate(hourlyDiff = c(0, abs(diff(hourlyMeanTemp)))) %>% 
+    filter(hourlyDiff > hourly_change_value) %>% 
+    distinct(sampleDate) %>% 
+    pull(sampleDate)
+  data %>% 
+    mutate(flag_hourlyChange = ifelse(sampleDate %in% dates_to_flag, 1, 0))
+}
+
+flag_daily_change <- function(data, daily_change_value = 3) {
+  dates_to_flag <- data %>% 
+    group_by(sampleDate) %>% 
+    summarize(dailyMeanTemp = mean(Temperature, na.rm = TRUE)) %>% 
+    mutate(dailyDiff = c(0, abs(diff(dailyMeanTemp)))) %>% 
+    filter(dailyDiff > daily_change_value) %>% 
+    distinct(sampleDate) %>% 
+    pull(sampleDate)
+  data %>% 
+    mutate(flag_dailyChange = ifelse(sampleDate %in% dates_to_flag, 1, 0))
+}
+
+add_temp_flags <- function(data) {
+  data %>% 
+    flag_high_temps() %>% 
+    flag_low_temps() %>% 
+    flag_hourly_change() %>% 
+    flag_daily_change()
+} 
+
+plot_flags <- function(data) {
+  p1 <- data %>%
+    ggplot(aes(x = dateTime, y = Temperature)) +
+    geom_line(aes(color = flag_highTemps)) + 
+    theme(legend.position = "bottom") +
+    labs(title = "Temperature measurements > 25 C")
+  
+  p2 <- data %>% 
+    ggplot(aes(x = dateTime, y = Temperature)) +
+    geom_line(aes(color = flag_lowTemps)) + 
+    theme(legend.position = "bottom") +
+    labs(title = "Temperature measurements < -1 C")
+  
+  p3 <- data %>% 
+    ggplot(aes(x = dateTime, y = Temperature)) +
+    geom_line(aes(color = flag_hourlyChange)) + 
+    theme(legend.position = "bottom") +
+    labs(title = "Rate of change in hourly mean temps. > 3 C")
+  
+  p4 <- data %>% 
+    ggplot(aes(x = dateTime, y = Temperature)) +
+    geom_line(aes(color = flag_dailyChange)) + 
+    theme(legend.position = "bottom") +
+    labs(title = "Rate of change in daily mean temps. > 3 C")
+  
+  grid.arrange(p1, p2, p3, p4, nrow = 2)
+  
+}
+
+
+
+APUdat %>% 
+  filter(Site == "APU9") %>% 
+  add_temp_flags() %>% 
+  plot_flags()
+
+APUdat %>% 
+  filter(Site == "APU9") %>% 
+  add_temp_flags() %>% 
+  filter(sampleDate > "2015-08-01",
+         sampleDate < "2015-09-15") %>% 
+  ggplot(aes(x = dateTime, y = Temperature)) +
+  geom_line(aes(color = flag_hourlyChange)) + 
+  theme(legend.position = "bottom") 
 
 #data arguments don't take a default, but detail args do.
 #... ellipsis allows passing argument to other functions.
 #checks on user inputs should be at start of function body
 #assertive package for errors on argument inputs
-
-if(!is.numeric(x)){
-  stop("x is not numeric")
-}
-
-assert_is_numeric(x)
 
 #library(zeallot)
 # multi-assignment operator %<-%
